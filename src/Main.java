a6b434676040a669a4593ac98cfa92ca93224d47
Pranav S Nair <pranavsnair2028@cs.sjcetpalai.ac.in>
2025-07-18 09:22:03 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,7 @@ public void displayDetails(){
 }
 }
 public class Main {
-
-    // TODO: Create a class called Student with name, rollNumber, and grade
-
-    // TODO: Create a method to display student details
-
     public static void main(String[] args) {
-        // TODO: Create a Student object and call the method to display details
         Student john = new Student();
         john.displayDetails();
     }
